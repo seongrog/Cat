c3de1cae26552e394b9f8222feb073a9e2841384
Solution Architect <8054390+seongrog@users.noreply.github.com>
2021-06-03 20:08:49 -0700

    Add files via upload

---
+++ SiemensHelloWorld.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *  Compilation:  javac SiemensHelloWorld.java
+ *  Execution:    java SiemensHelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java SiemensHelloWorld
+ *  Hello, SiemensHelloWorld
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class SiemensHelloWorld {
+
+    public static void main(String[] args) {
+        /*
+         *  Add source code to Work Item EL-159
+         *  Link: http://localhost/polarion/#/project/elibrary/workitems/requirement?query=NOT%20HAS_VALUE%3Aresolutio
+         *  @wi elibrary/EL-159 
+        */
+        System.out.println("Hello, Siemens World!"); // Prints "Hello, Siemens World!" to the terminal window.
+    }
+
+} 
